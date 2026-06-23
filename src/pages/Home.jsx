6f10880233b11ad4062c2e43fae4f48be20b4eb1
zainab845 <zainabbilal597@gmail.com>
2026-06-23 15:53:25 +0500

    added projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import Hero from '../sections/Hero'
 import About from '../sections/About'
+import Projects from '../sections/Projects'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+        <Projects />
+        
     </main>
   )
 }
