414a531df81696812284d42d4a41c9653949e2d2
zainab845 <zainabbilal597@gmail.com>
2026-06-23 16:09:03 +0500

    added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import Hero from '../sections/Hero'
 import About from '../sections/About'
 import Projects from '../sections/Projects'
+import Skills from '../sections/Skills'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Skills />
         <Projects />
+        
         
     </main>
   )
